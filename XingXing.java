62f4a22365451ea1b0233ac1f1fd733e0d501dc4
hou199312 <50061453+hou199312@users.noreply.github.com>
2019-07-28 01:00:40 +0800

    Add files via upload

---
+++ XingXing.java
@@ -0,0 +1,10 @@
+public class XingXing{
+	public static void main(String[] args){
+		for(int lie =1;lie<=5;lie++){
+			for(int hang =1; hang<=5;hang++){
+			System.out.print("*");	
+			}
+		System.out.println();	
+		}
+	}
+}
